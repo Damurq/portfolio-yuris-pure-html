9a001ac358ee7ab7c60cdcacc030348defd2293d
damurq <michaelmontero.idb@gmail.com>
2022-03-12 13:48:38 -0400

    portfolio section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+
 /*==================== MENU SHOW Y HIDDEN ====================*/
 const navMenu = document.getElementById("nav-menu"),
     navToggle = document.getElementById("nav-toggle"),
@@ -89,7 +90,18 @@ Array.from(modalCloses).forEach((modalClose) => {
     })
 })
 /*==================== PORTFOLIO SWIPER  ====================*/
-
+let swiper = new Swiper('.portfolio__container', {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    }
+  });
 
 /*==================== TESTIMONIAL ====================*/
 
